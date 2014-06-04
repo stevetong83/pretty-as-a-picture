c505d3ac916b7ab682b171829bd87c74d2c573cf
Stephen Tong <stevetong83@hotmail.com>
2014-06-04 12:50:30 -0700

    added UML

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,7 +1,8 @@
-class LineItem < ActiveRecord::Base
+lineclass LineItem < ActiveRecord::Base
 
   belongs_to :product
   belongs_to :cart
+  belongs_to :order
 
   validates :product, :cart, presence: true
   validates :product_id, uniqueness: { scope: :cart_id }

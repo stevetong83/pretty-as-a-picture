65e3840f5371f01e0fcfc92c6afc8adbf4bd38a1
Stephen Tong <stephentong@Stephens-MacBook-Pro-2.local>
2014-09-19 14:12:25 -0700

    contact form

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,10 +2,11 @@ class MessagesController < ApplicationController
 
   def new
     @message = Message.new
+    @content = Page.where(page: "contact").first
   end
 
   def create
-    @message = Message.create params message_params
+    @message = Message.create message_params
     if @message.save
       redirect_to root_path, notice: "Thank you for contacting us! We get in touch with you as soon as possible."
     else
